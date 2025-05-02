6c563fcbee86a404cee22bdde2932a8472a70a30
Grace Pontanar <gracepontanar@gmail.com>
2025-05-03 01:38:06 +0800

    Adding Vercel URL in environment variable

--- index.js
+++ index.js
@@ -14,8 +14,20 @@ const app = express();
 
 // Enable CORS for specific origin
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: [
+        'http://localhost:3000', // For local development
+        'https://blog-post-steel-gamma.vercel.app/', // Your Vercel frontend URL
+    ],
 }));
+
+// OR CAN USE THIS: but add the key and value in render environment variables
+    // Key: ALLOWED_ORIGINS
+    // Value: http://localhost:3000,https://blog-post-steel-gamma.vercel.app/
+// const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
+
+// app.use(cors({
+//     origin: allowedOrigins,
+// }));
 
 // Connecting to MongoDB Atlas
 mongoose.connect(process.env.MONGODB_STRING, {
